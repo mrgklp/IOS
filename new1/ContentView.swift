776f220576fbb3ec0eb716d4541360ea9d07df7f
mrgklp <ahmetemregokalp@gmail.com>
2022-09-01 09:31:22 +0300

    Initial Commit

---
+++ new1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  new1
+//
+//  Created by Ahmet Emre Gökalp on 1.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ new1/new1App.swift
@@ -0,0 +1,17 @@
+//
+//  new1App.swift
+//  new1
+//
+//  Created by Ahmet Emre Gökalp on 1.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct new1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
